19fc94fd093d3558c2f01e8815e9730005eb218d
NeoEvaX <ian.kremer@gmail.com>
2024-07-27 11:58:56 -0700

    Adds user parsing and getting from command

--- bot/bot.go
+++ bot/bot.go
@@ -1,10 +1,13 @@
 package bot
 
 import (
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
+	"slices"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -152,7 +155,6 @@ func newMessage(s *discordgo.Session, message *discordgo.MessageCreate) {
 
 		slog.Info("BrokenMessage", slog.Any("Message", brokenMessage))
 		if !verifySetupGameMessage(brokenMessage) {
-
 			s.ChannelMessageSend(
 				message.ChannelID,
 				"Command formatted poorly. Try again: "+
@@ -164,6 +166,24 @@ func newMessage(s *discordgo.Session, message *discordgo.MessageCreate) {
 			return
 		}
 		gameName := brokenMessage[1]
+		gameMasters, err := parseGamemasters(s, brokenMessage)
+		if err != nil {
+			return
+		}
+
+		gamePlayers, err := parsePlayers(s, brokenMessage)
+		if err != nil {
+			return
+		}
+
+		slog.Info("Found Gamemasters", slog.Any("gameMasters", gameMasters))
+		slog.Info("Found Players", slog.Any("gamePlayers", gamePlayers))
+
+		everyoneRole, err := findRole(s, "@everyone")
+		if err != nil {
+			slog.Error("Error finding @everyone role", slog.Any("err", err))
+			return
+		}
 
 		channels, err := s.GuildChannels(GuildID)
 		if err != nil {
@@ -177,7 +197,13 @@ func newMessage(s *discordgo.Session, message *discordgo.MessageCreate) {
 			return
 		}
 		slog.Info(fmt.Sprintf("Creating Category: %s", gameName))
-		newCategory, err := s.GuildChannelCreate(message.GuildID, gameName, discordgo.ChannelTypeGuildCategory)
+		newCategoryPermissionOverrides := []*discordgo.PermissionOverwrite{
+			{ID: gameMasters[0].User.ID, Type: discordgo.PermissionOverwriteTypeMember, Allow: discordgo.PermissionManageMessages, Deny: 0},
+			{ID: gameMasters[0].User.ID, Type: discordgo.PermissionOverwriteTypeMember, Allow: discordgo.PermissionManageChannels, Deny: 0},
+			{ID: everyoneRole.ID, Type: discordgo.PermissionOverwriteTypeRole, Allow: 0, Deny: discordgo.PermissionViewChannel},
+		}
+		newCategoryData := discordgo.GuildChannelCreateData{Name: gameName, Type: discordgo.ChannelTypeGuildCategory, PermissionOverwrites: newCategoryPermissionOverrides}
+		newCategory, err := s.GuildChannelCreateComplex(message.GuildID, newCategoryData)
 		slog.Info("Done")
 
 		if err == nil {
@@ -253,4 +279,64 @@ func doesCategoryExist(channels []*discordgo.Channel, newName string) bool {
 		}
 	}
 	return false
+}
+
+func findMemberFromName(s *discordgo.Session, userId string) (member *discordgo.Member, err error) {
+	userId = strings.Replace(userId, "<@", "", -1)
+	userId = strings.Replace(userId, ">", "", -1)
+	member, err = s.GuildMember(GuildID, userId)
+	if err != nil {
+		slog.Error("Error finding member", slog.String("userId", userId), slog.Any("err", err))
+		return
+	}
+	return
+}
+
+func parseGamemasters(s *discordgo.Session, message []string) ([]discordgo.Member, error) {
+	var gameMasters []discordgo.Member
+	// start looping through after GM tag
+	for _, a := range message[3:] {
+		if a == "Players" {
+			return gameMasters, nil
+		}
+		newGameMaster, err := findMemberFromName(s, a)
+		if err != nil {
+			slog.Error("Error finding member", slog.String("userId", a), slog.Any("err", err))
+			return nil, err
+		}
+		gameMasters = append(gameMasters, *newGameMaster)
+	}
+	return gameMasters, nil
+}
+
+func parsePlayers(s *discordgo.Session, message []string) ([]discordgo.Member, error) {
+	var players []discordgo.Member
+	// start looping through after GMs, but skipping till its Players
+	started := false
+	for _, a := range message[4:] {
+		if a == "Players" {
+			started = true
+			continue
+
+		}
+		if !started {
+			continue
+		}
+		newPlayer, err := findMemberFromName(s, a)
+		if err != nil {
+			return nil, err
+		}
+		players = append(players, *newPlayer)
+	}
+	return players, nil
+}
+
+func findRole(s *discordgo.Session, roleName string) (discordgo.Role, error) {
+	// Get the @everyone role
+	Roles, _ := s.GuildRoles(GuildID)
+	idx := slices.IndexFunc(Roles, func(r *discordgo.Role) bool { return r.Name == roleName })
+	if idx == -1 {
+		return discordgo.Role{}, errors.New("Could not find role")
+	}
+	return *Roles[idx], nil
 }
